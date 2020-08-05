966a9b91f00e38bfe484d06d457ecc3be3560c25
Danil Solovjov <mr.danechek0@gmail.com>
2020-08-05 23:27:27 +0300

    Changed constructor forms and added brief documentation.

--- src/main/java/com/matrix_java/TestMatrix.java
+++ src/main/java/com/matrix_java/TestMatrix.java
@@ -5,16 +5,8 @@ import java.util.*;
 public class TestMatrix {
     public static void main(String[] args) {
         Matrix<Integer> matrix = new Matrix<>(new Integer[][]{{1, 2, 3}, {4, 5, 6}});
-
-        Matrix<Integer> matrix1 = matrix.clone();
-
-
         System.out.println(matrix);
-        System.out.println(matrix1);
-
-        matrix1.set(0,0, 255);
-
+        Matrices.transpose(matrix);
         System.out.println(matrix);
-        System.out.println(matrix1);
     }
 }
