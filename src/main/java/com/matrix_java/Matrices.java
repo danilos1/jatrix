97a9d63dc94536c61c870b6b8a0a8682589683f3
Danil Solovjov <mr.danechek0@gmail.com>
2020-08-05 18:45:18 +0300

    Changed constructor forms and added brief documentation.

---
+++ src/main/java/com/matrix_java/Matrices.java
@@ -0,0 +1,25 @@
+package com.matrix_java;
+
+import java.util.Comparator;
+
+/**
+ * <code>Matrices</code> class is intended for performing basic matrix operations such as
+ * adding, multiplication, searching, etc.
+ */
+public class Matrices {
+    
+    private Matrices() {}
+    
+    public static Matrix add(Matrix m1, Matrix m2) { return null; }
+    
+    public static Matrix add(Matrix dest, Matrix m1, Matrix m2) { return null;}
+    
+    public static Matrix mul(Matrix m1, Matrix m2) { return null; }
+    
+    public static Matrix mul(Matrix dest, Matrix m1, Matrix m2) { return null; }
+    
+    public static void sort() {}
+    
+    public static void sort(Matrix matrix, Comparator comparator) { }
+   
+}
