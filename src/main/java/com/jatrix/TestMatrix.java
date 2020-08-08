7331e54d4bdd8a942a535016ac59f3e9dfbe0ed2
Dima2057 <dima.tumochko@gmail.com>
2020-08-08 16:03:14 +0300

    Added summation method of 2 matrices

--- src/main/java/com/jatrix/TestMatrix.java
+++ src/main/java/com/jatrix/TestMatrix.java
@@ -2,7 +2,10 @@ package com.jatrix;
 
 public class TestMatrix {
     public static void main(String[] args) {
+        Matrix a = new Matrix(new double[][] {{1,2,3}, {4, 5, 6}});
+        Matrix b = new Matrix(new double[][] {{6,5,4}, {3, 2, 1}});
+        Matrix c = Matrices.add(a, b);
 
-
+        System.out.println(c);
     }
 }
