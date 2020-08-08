8bb3a63361b30a282bc257c4464beb5bb05c7f21
Danil Solovjov <mr.danechek0@gmail.com>
2020-08-08 16:42:04 +0300

    Changed summation method

--- src/main/java/com/jatrix/TestMatrix.java
+++ src/main/java/com/jatrix/TestMatrix.java
@@ -2,7 +2,10 @@ package com.jatrix;
 
 public class TestMatrix {
     public static void main(String[] args) {
+        Matrix m1 = new Matrix(new double[][]{{1,2},{3,4}});
+        Matrix m2 = new Matrix(new double[][]{{5,8},{3,2}});
 
-
+        Matrix m3 = Matrices.add(Matrices.add(m1, m2), m2);
+        System.out.println(m3);
     }
 }
