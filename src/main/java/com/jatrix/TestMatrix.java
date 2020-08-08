e6df0931c7bca63da5c98e84ae2ea536949dace9
Danil Solovjov <mr.danechek0@gmail.com>
2020-08-09 01:37:16 +0300

    Tested some ops with streams

--- src/main/java/com/jatrix/TestMatrix.java
+++ src/main/java/com/jatrix/TestMatrix.java
@@ -1,15 +1,12 @@
 package main.java.com.jatrix;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Spliterator;
+import java.util.*;
 
 public class TestMatrix {
     public static void main(String[] args) {
-        Matrix m1 = new Matrix(new double[][]{{1, 2, 4.3}, {4, 5.6, 3.3}, {1.9, 4.78, 6.12}});
+        Matrix m1 = new Matrix(new double[][]{{1, 0.005, 4.3}, {4, 5.6, 3.3}, {1.9, 4.78, 6.12}});
 
-        m1.stream().map((val) -> val * 100).forEach(System.out::println);
-
+        m1.stream().max().ifPresent((val) -> System.out.printf("Max value is %.3f%n",val));
+        m1.stream().min().ifPresent((val) -> System.out.printf("Min value is %.3f%n",val));
     }
 }
