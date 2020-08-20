ec9de34168b389d66ff03eb5d315919e14d076c1
Danil Solovjov <mr.danechek0@gmail.com>
2020-08-20 10:24:36 +0300

    Updated

--- src/main/java/com/jatrix/TestMatrix.java
+++ src/main/java/com/jatrix/TestMatrix.java
@@ -7,7 +7,7 @@ import java.util.Random;
 
 public class TestMatrix {
     public static void main(String[] args) {
-        int N = 30;
+        int N = 50;
         Matrix newMatrix = new Matrix(N);
         Matrices.fillRandom(newMatrix);
 
