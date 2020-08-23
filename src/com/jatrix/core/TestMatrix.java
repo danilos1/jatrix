b03dfb190348c9da260a141a0029ba2b42d69244
Danil Solovjov <mr.danechek0@gmail.com>
2020-08-24 01:39:55 +0300

    Covered 63% Matrix tests

--- src/com/jatrix/core/TestMatrix.java
+++ src/com/jatrix/core/TestMatrix.java
@@ -1,8 +1,19 @@
 package com.jatrix.core;
 
+import java.util.Arrays;
+
 public class TestMatrix {
     public static void main(String[] args) {
-        Matrix matrix2 = new Matrix(3,-4);
+        Matrix matrix = new Matrix(new double[][]{
+                {1, -9.6, 3, 5.7},
+                {-1, 4, 4, 9},
+        });
+        double[] expected = {1, -9.6, 3, 5.7,-1, 4, 4, 9};
 
+        // Act
+        double[] actual = matrix.stream().toArray();
+
+
+        System.out.println(Arrays.toString(actual));
     }
 }
